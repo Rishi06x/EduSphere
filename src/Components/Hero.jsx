14d62c69763866653f15b0421bba827fe11ba792
Rishi <rishiranjith1234@gmail.com>
2026-01-09 21:45:49 +0530

    Updated the Hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,6 @@
 import './Hero.css';
 import image from '../assets/background.webp';
+import rightArrow from '../assets/right-arrow.png'
 
 const Hero = () => {
     return (
@@ -11,7 +12,7 @@ const Hero = () => {
             <div className="hero-content">
                 <h1>We Ensure better education for a better world</h1>
                 <h4>Our cutting edge  curriculum is designed to empower students with the knowledge, skills and experience needed to excel in the dynamic field of education </h4>
-                <button>➡️ Explore Now</button>
+                <button>Explore Now <img src={rightArrow} alt="right arrow" id='rightArrow'/></button>
             </div>
         </div>
     );
